08da848f3966aa5344002e99b3977f6bd6535d10
sadayuki-matsuno <sadayuki.matsuno@gmail.com>
2025-08-10 19:19:32 +0900

    change room name

--- backend/pkg/db/insert.go
+++ backend/pkg/db/insert.go
@@ -27,7 +27,7 @@ func InsertSampleRecord(db *gorm.DB) error {
 	description := "どんな話題でもOK!　雑談ルーム"
 	room := Room{
 		Description: &description,
-		Name:        "#general",
+		Name:        "general",
 	}
 
 	session := Session{
